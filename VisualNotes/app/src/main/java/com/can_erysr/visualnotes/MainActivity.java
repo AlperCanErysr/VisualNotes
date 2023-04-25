f2f6ac52af1b203f1f99fb577ba2635853529af0
Alper Can Eryasar <a.can.erysr@gmail.com>
2023-04-25 14:01:11 +0300

    VisualNotes

--- VisualNotes/app/src/main/java/com/can_erysr/visualnotes/MainActivity.java
+++ VisualNotes/app/src/main/java/com/can_erysr/visualnotes/MainActivity.java
@@ -2,13 +2,38 @@ package com.can_erysr.visualnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.newnote);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewNote();
+            }
+        });
+    }
+
+    public void openNewNote(){
+        Intent intent = new Intent(this,NewNoteActivity.class);
+        startActivity(intent);
+    }
+
+    public void newnote (View view){
+
+    }
+    public void notes (View view){
+
     }
 }
