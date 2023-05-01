fc103f299c78d8e207a8ae53ed05e08ff6db0b4f
Alper Can Eryasar <a.can.erysr@gmail.com>
2023-05-01 21:55:26 +0300

    VisualNotes

--- VisualNotes/app/src/main/java/com/can_erysr/visualnotes/NewNoteActivity.java
+++ VisualNotes/app/src/main/java/com/can_erysr/visualnotes/NewNoteActivity.java
@@ -1,13 +1,19 @@
 package com.can_erysr.visualnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.FileOutputStream;
 
@@ -21,7 +27,24 @@ public class NewNoteActivity extends AppCompatActivity {
 
     }
 
+
     public void selectImage (View view){
- 
+        //RESİM İZİN KONTROLÜ
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }).show();
+            }
+            else{
+
+            }
+        }
+        else {
+             
+        }
     }
 }
