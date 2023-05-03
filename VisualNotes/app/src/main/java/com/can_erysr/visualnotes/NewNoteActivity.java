ccbacc66ae0598620d63fcaff64e23371a0002f3
Alper Can Eryasar <a.can.erysr@gmail.com>
2023-05-03 13:59:11 +0300

    VisualNotes

--- VisualNotes/app/src/main/java/com/can_erysr/visualnotes/NewNoteActivity.java
+++ VisualNotes/app/src/main/java/com/can_erysr/visualnotes/NewNoteActivity.java
@@ -1,29 +1,48 @@
 package com.can_erysr.visualnotes;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.can_erysr.visualnotes.databinding.ActivityNewNoteBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.FileOutputStream;
 
 public class NewNoteActivity extends AppCompatActivity {
 
+    private ActivityNewNoteBinding binding;
+    Bitmap selectedImage;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_note);
+        binding = ActivityNewNoteBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        registerLauncher();
 
     }
 
@@ -44,7 +63,51 @@ public class NewNoteActivity extends AppCompatActivity {
             }
         }
         else {
-             
+             Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         }
+    }
+
+    private void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK){
+                    Intent intentFromResult = result.getData();
+
+                    if (intentFromResult != null){
+                        Uri imageData = intentFromResult.getData();
+                        //binding.imageView.setImageURI(imageData);
+
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28){
+                                ImageDecoder.Source source = ImageDecoder.createSource(NewNoteActivity.this.getContentResolver(),imageData);
+                                Bitmap selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+                            else {
+                                selectedImage = MediaStore.Images.Media.getBitmap(NewNoteActivity.this.getContentResolver(),imageData);
+                                binding.imageView.setImageBitmap(selectedImage);
+                            }
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result){
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                }
+                else{
+                    Toast.makeText(NewNoteActivity.this,"Permission needed!!!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
