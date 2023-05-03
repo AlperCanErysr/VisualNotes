edc6e3e01507e95e942068f984d0d07e27da7b2b
Alper Can Eryasar <a.can.erysr@gmail.com>
2023-05-03 18:35:19 +0300

    VisualNotes

--- VisualNotes/app/src/main/java/com/can_erysr/visualnotes/MainActivity.java
+++ VisualNotes/app/src/main/java/com/can_erysr/visualnotes/MainActivity.java
@@ -3,15 +3,13 @@ package com.can_erysr.visualnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn;
 
+    ArrayList<Notes> notesArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notesArrayList = new ArrayList<>();
+        getData();
 
         //BUTONA TIKLANDIĞINDA NE OLACAK -1
         button = (Button) findViewById(R.id.newnote);
@@ -42,6 +44,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void getData(){
+        try {
+            SQLiteDatabase sqLiteStatement = this.openOrCreateDatabase("Notes",MODE_PRIVATE,null);
+
+            Cursor cursor = sqLiteStatement.rawQuery("SELECT * FROM notes",null);
+
+            int titleIx = cursor.getColumnIndex("title");
+            int noteIx = cursor.getColumnIndex("note");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()){
+                String title = cursor.getString(titleIx);
+                String note = cursor.getString(noteIx);
+
+                int id = cursor.getInt(idIx);
+
+                Notes notes = new Notes(title,note,id);
+                notesArrayList.add(notes);
+            }
+            cursor.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     //BUTONA TIKLANDIĞINDA NE OLACAK BAŞKA BİR AKTİVİTEYE GİDECEK -1
     public void openNewNote(){
         Intent intent = new Intent(this,NewNoteActivity.class);
---
+++ VisualNotes/app/src/main/java/com/can_erysr/visualnotes/Notes.java
@@ -0,0 +1,14 @@
+package com.can_erysr.visualnotes;
+
+public class Notes {
+
+    String title;
+    String notes;
+    int id;
+
+    public Notes(String title, String notes, int id) {
+        this.title = title;
+        this.notes = notes;
+        this.id = id;
+    }
+}
